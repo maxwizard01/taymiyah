128b6698851104a271711a6c03eff388372b1ee3
Oladejo Abdullahi <67700908+maxwizard01@users.noreply.github.com>
2025-06-14 21:30:54 +0100

    Add files via upload

--- script/indexElement.js
+++ script/indexElement.js
@@ -10,7 +10,7 @@ a=`<div class="col-10 col-md-6 col-lg-4 col-xl-3">
                   <img src="../Quiz Images/${listOfImage[i]}">
                   <div class="card-body text-center">
                     <h5 class="card-title">${listOfSubject[i]}</h5>
-                    <p class="card-text">Are you a ${listOfSubject[i]} Ninja ? Check by attempting the quiz</p>
+                    <p class="card-text">Are you  ready for  ${listOfSubject[i]} quiz ? Attempting the quiz by clicking Launch</p>
                       
                         <button type="button" class="mybtn" data-bs-toggle="modal"  data-bs-target='#staticBackdrop${i}'>
                           Launch  <i class="bi bi-send"></i>
@@ -30,10 +30,10 @@ a=`<div class="col-10 col-md-6 col-lg-4 col-xl-3">
                               
                               <div class="modal-body"  style="text-align: left;">
                                 <ul>
-                                  <li>Number of questions : 30</li>
-                                   <li>Check the top right corner to use Calculator <br> [Calculator best displayed on large screen devices]</li>
-                                   <li> <i class="bi bi-clock bg-light"></i> Time allotted : 30 minutes</li>
-                                   <li>The answers become <strong>sticky</strong> on select, implying that you can not change an answer once selected. Hence, only the <strong>Next</strong> button to proceed is available.</li>
+                                    <li> <i class="bi bi-clock bg-light"></i> Time allotted : 30 minutes</li>
+                                   <li>The answers become <strong>selected</strong> on select, implying that you can  change an answer once the option selected. The <strong>Next</strong> button to proceed is available.</li>
+                                   <li> Use the <strong>Next</strong> button to proceed to next question vailable. and <b> Previous </b> to previous question</li>
+                                   <li>Clicking submit button is irreversible. Meaning once you click submit you can't undo it.</li>
                                    <li>Automatic submission once the time lapse.</li>
                                  </ul>
                               </div>
